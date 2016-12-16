5458c3e12fbb2b90d6b03a5cd49b30bb30b5f4fe
sgtquezada <sgtquezada@gmail.com>
2016-12-16 15:57:04 -0800

    added put route

--- blogPostsRouter.js
+++ blogPostsRouter.js
@@ -35,4 +35,37 @@ router.post('/', jsonParser, (req, res) => {
     res.status(200).json(post);
 });
 
+// delete a single post
+router.delete('/:id', (req, res) => {
+  let postId = req.params.id;
+  console.log(`deleting post with id: ${postId}`);
+  BlogPosts.delete(postId);
+  res.status(204).end();
+});
+
+// update a single post
+router.put('/:id', jsonParser, (req, res) => {
+  let requiredFields = ['title', 'content', 'author', 'publishDate'];
+  for (let i = 0; i < requiredFields.length; i += 1) {
+    let field = requiredFields[i];
+    if (!(field in req.body)) {
+      console.log("Bad request. You are missing required fields");
+      return res.status(400).end();
+    }
+  }
+  if (req.body.id !== req.params.id) {
+    console.log(`PostId: ${req.body.id} in body req does not match id: ${req.params.id} passed in the path`);
+    let message = 'Nice try! The post id passed in the request does not match the id passed in the URL'
+    return res.status(400).send(message);
+  }
+  let updatedPost = BlogPosts.update(req.body);
+  res.status(200).json(updatedPost);
+});
+
+
+
+
+
+
+
 module.exports = router;
