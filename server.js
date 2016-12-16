b8c104f981c2537d4fd7b61340f14942e1664e72
sgtquezada <sgtquezada@gmail.com>
2016-12-16 15:10:27 -0800

    added post route

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const jsonParser = bodyParser.json();
 const morgan = require('morgan');
 const app = express();
 
+const {BlogPosts} = require('./models');
+const BlogPostsRouter = require('./blogPostsRouter');
 
-app.get('/', (req, res) => {
-  res.send('you found me!');
-});
+
+app.use('/blog-posts', BlogPostsRouter);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`app listen on ${process.env.PORT} || 8080`);
