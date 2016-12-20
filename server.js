b916049e516cb92befd4ce711ebb8a652fa3f5dc
sgtquezada <sgtquezada@gmail.com>
2016-12-19 21:41:16 -0800

    added travis integration

--- server.js
+++ server.js
@@ -10,6 +10,41 @@ const BlogPostsRouter = require('./blogPostsRouter');
 
 app.use('/blog-posts', BlogPostsRouter);
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`app listen on ${process.env.PORT} || 8080`);
-});
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+// like `runServer`, this function also needs to return a promise.
+// `server.close` does not return a promise on its own, so we manually
+// create one.
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
